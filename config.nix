352c384c7f95facc13287c2efd5cde10dde02c49
asa1984 <satoasa9913@gmail.com>
2024-12-13 01:41:36 +0900

    feat: integrate lazy.nvim with Nix by not hacky way

--- config.nix
+++ config.nix
@@ -6,65 +6,38 @@
 }:
 let
   envVars = plugins // {
-    treesitter_parsers =
-      lib.pipe
-        (vimPlugins.nvim-treesitter.withPlugins (
-          p: with p; [
-            astro
-            bash
-            c
-            comment
-            cpp
-            css
-            dockerfile
-            go
-            gomod
-            graphql
-            haskell
-            html
-            javascript
-            jsdoc
-            json
-            jsonc
-            lua
-            make
-            markdown
-            markdown_inline # lspsaga.nvim require this
-            nix
-            ocaml
-            prisma
-            python
-            rust
-            scss
-            sql
-            toml
-            tsx
-            typescript
-            typst
-            vim
-            yaml
-            zig
-          ]
-        )).dependencies
-        [
-          (builtins.map builtins.toString)
-          (builtins.concatStringsSep ",")
-        ];
+    treesitter_parsers = lib.pipe (builtins.attrValues vimPlugins.nvim-treesitter-parsers) [
+      (builtins.map builtins.toString)
+      (builtins.concatStringsSep ",")
+    ];
   };
-in
-stdenv.mkDerivation (
-  envVars
-  // {
-    pname = "asa1984-nvim-config";
-    version = "latest";
-    src = ./nvim;
+  preprocessed = stdenv.mkDerivation (
+    envVars
+    // {
+      pname = "asa1984-nvim-config";
+      version = "latest";
+      src = ./nvim;
 
-    installPhase = ''
-      mkdir -p $out
-      for file in $(find . -type f); do
-        substituteAllInPlace "$file"
-      done
-      cp -r ./ $out
-    '';
-  }
-)
+      installPhase = ''
+        mkdir -p $out
+        for file in $(find . -type f); do
+          substituteAllInPlace "$file"
+        done
+        cp -r ./ $out
+      '';
+    }
+  );
+in
+stdenv.mkDerivation {
+  pname = "asa1984-nvim-config";
+  version = "latest";
+  src = preprocessed;
+
+  asa1984_nvim_config_path = preprocessed;
+
+  installPhase = ''
+    mkdir -p $out
+    substituteAllInPlace ./init.lua
+    cp -r ./ $out
+  '';
+}
