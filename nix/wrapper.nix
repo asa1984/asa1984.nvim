51403dee77212a8c4953633766ddaa68e7836727
asa1984 <satoasa9913@gmail.com>
2024-12-13 14:55:03 +0900

    fix(build): stop using overlays to avoid build errors for neovim-nightly on aarch64-darwin

--- nix/wrapper.nix
+++ nix/wrapper.nix
@@ -1,9 +1,9 @@
-pkgs: extraPackages:
+neovim: pkgs: extraPackages:
 let
   plugins = import ./plugins.nix pkgs;
   nvimConfig = pkgs.callPackage ./config.nix { inherit plugins; };
 in
 pkgs.writeShellScriptBin "nvim" ''
   PATH=$PATH:${pkgs.lib.makeBinPath extraPackages}
-  MY_CONFIG_PATH=${nvimConfig} ${pkgs.neovim}/bin/nvim -u ${nvimConfig}/init.lua "$@"
+  MY_CONFIG_PATH=${nvimConfig} ${neovim}/bin/nvim -u ${nvimConfig}/init.lua "$@"
 ''
