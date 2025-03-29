55a4211a8af6daf0b112035b6354806614b5c3e9
asa1984 <satoasa9913@gmail.com>
2025-03-29 11:01:11 +0900

    fix: resolve the problem failing to load tiny-inline-diagnostic.nvim

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,13 +1,11 @@
 local formatter = require("plugins.lsp.formatter")
 local lsp = require("plugins.lsp.lsp")
 local misc = require("plugins.lsp.misc")
-local ui = require("plugins.lsp.ui")
 
 table.unpack = table.unpack or unpack -- 5.1 compatibility
 
 return {
     table.unpack(formatter),
     table.unpack(lsp),
-    table.unpack(ui),
     table.unpack(misc),
 }
