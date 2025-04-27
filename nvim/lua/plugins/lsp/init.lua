f83af85771ca30355df25c969eebf014527f244e
asa1984 <satoasa9913@gmail.com>
2025-04-27 16:55:38 +0900

    refactor: separate conform.nvim config

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,11 +1,9 @@
-local formatter = require("plugins.lsp.formatter")
 local lsp = require("plugins.lsp.lsp")
 local misc = require("plugins.lsp.misc")
 
 table.unpack = table.unpack or unpack -- 5.1 compatibility
 
 return {
-    table.unpack(formatter),
     table.unpack(lsp),
     table.unpack(misc),
 }
