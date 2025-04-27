1ed1d7cde34dc1a8437963f14263c0153c64a255
asa1984 <satoasa9913@gmail.com>
2025-04-27 16:54:10 +0900

    refactor: separate tailwind-tools.nvim config

--- nvim/lua/plugins/lsp/misc.lua
+++ nvim/lua/plugins/lsp/misc.lua
@@ -23,15 +23,4 @@ return {
             },
         },
     },
-
-    -- Tailwind tools
-    {
-        name = "tailwind-tools.nvim",
-        dir = "@tailwind_tools_nvim@",
-        ft = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte", "vue" },
-        dependencies = {
-            { name = "nvim-treesitter", dir = "@nvim_treesitter@" },
-        },
-        opts = {},
-    },
 }
