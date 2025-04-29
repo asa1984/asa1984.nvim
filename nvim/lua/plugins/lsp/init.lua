e9b001585af4642151e767309d40447c6643a4c4
asa1984 <satoasa9913@gmail.com>
2025-04-29 23:24:59 +0900

    feat: migrate nvim-lspconfig for nvim 0.11

--- nvim/lua/plugins/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local lsp = require("plugins.lsp.lsp")
-local misc = require("plugins.lsp.misc")
-
-table.unpack = table.unpack or unpack -- 5.1 compatibility
-
-return {
-    table.unpack(lsp),
-    table.unpack(misc),
-}
