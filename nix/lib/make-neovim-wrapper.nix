bf26fb88afe94e89590193618527bd66f24e29a1
asa1984 <satoasa9913@gmail.com>
2025-10-23 01:07:39 +0900

    fix: treesitter parsers

--- nix/lib/make-neovim-wrapper.nix
+++ nix/lib/make-neovim-wrapper.nix
@@ -7,13 +7,9 @@
 }:
 let
   plugins = import ../plugins.nix pkgs;
-  extraWrapperArgs = [
-    "--suffix"
-    "PATH"
-    ":"
-    (pkgs.lib.makeBinPath tools)
-  ];
-  preprocessed = pkgs.stdenv.mkDerivation (
+
+  # Neovim Lua configuration with `@plugin@` placeholders substituted with actual store paths
+  luaConfig = pkgs.stdenv.mkDerivation (
     plugins
     // {
       pname = "asa1984-nvim-config";
@@ -29,17 +25,39 @@ let
       '';
     }
   );
+
+  # Comma-separated string of Neovim Treesitter parser store paths
+  treesitterParserPathsString =
+    let
+      treesitterParsers = builtins.attrValues pkgs.vimPlugins.nvim-treesitter-parsers;
+      isDerivation = p: p ? type && p.type == "derivation";
+    in
+    pkgs.lib.pipe treesitterParsers [
+      (builtins.filter isDerivation)
+      (builtins.map builtins.toString)
+      (builtins.concatStringsSep ",")
+    ];
+
   config = pkgs.neovimUtils.makeNeovimConfig {
     withNodeJs = true;
     withRuby = false;
     withPython3 = false;
     vimAlias = true;
-    customLuaRC = ''
-      vim.opt.rtp:prepend('${preprocessed}')
+    customLuaRC = # Lua
+      ''
+        vim.opt.rtp:prepend("${luaConfig}")
+        vim.opt.runtimepath:append("${treesitterParserPathsString}")
 
-      ${builtins.readFile "${preprocessed}/init.lua"}
-    '';
+        ${builtins.readFile "${luaConfig}/init.lua"}
+      '';
   };
+
+  extraWrapperArgs = [
+    "--suffix"
+    "PATH"
+    ":"
+    (pkgs.lib.makeBinPath tools)
+  ];
 in
 (pkgs.wrapNeovimUnstable neovim (
   config // { wrapperArgs = config.wrapperArgs ++ extraWrapperArgs; }
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,4 +2,10 @@ return {
     name = "nvim-treesitter",
     dir = "@nvim_treesitter@",
     event = "BufReadPre",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            auto_install = false,
+            highlight = { enable = true },
+        })
+    end,
 }
