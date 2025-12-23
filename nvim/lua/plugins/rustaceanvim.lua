11ce2e5e538f28c9bb30bd949247eba0a30ed8fc
asa1984 <satoasa9913@gmail.com>
2025-12-24 01:06:40 +0900

    fix(lsp): exclude several directories from rust-analyzer's analysis target

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -2,4 +2,17 @@ return {
     name = "rustaceanvim",
     dir = "@rustaceanvim@",
     lazy = false, -- This plugin is already lazy
+    init = function()
+        vim.g.rustaceanvim = {
+            server = {
+                default_settings = {
+                    ["rust-analyzer"] = {
+                        files = {
+                            excludeDirs = { "node_modules", ".direnv" },
+                        },
+                    },
+                },
+            },
+        }
+    end,
 }
